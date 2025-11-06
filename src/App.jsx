00d32c82aeb58b5fcca2fe04a7fd7c1aa9239cb8
Saurabh-yadav07 <syadav9718910607@gmail.com>
2025-11-06 14:37:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>Let's get started</h1>
+     <p>I am learning React</p> 
+    </>
+  )
+}
+
+export default App
