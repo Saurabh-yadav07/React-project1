3ab6acb934cd540229ddc6f923f751ac99cbffca
Saurabh-yadav07 <syadav9718910607@gmail.com>
2025-11-26 01:53:02 +0530

    Rendered Expense Form Conditionally

--- src/custom-components/NewExpense/NewExpense.jsx
+++ src/custom-components/NewExpense/NewExpense.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import {useState} from "react";
+import React  from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
-  const saveExpenseDataHandler = (enteredData) => {
-    const expenseData = {
-      ...enteredData,id:Math.random().toString()
-    }
+  const [isEditing, setIsEditing] = useState(false);
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = { ...enteredExpenseData, id: Math.random().toString() };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+  const startEditingHandler = () => {
+    setIsEditing(true)
+  };
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   }
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+  
+    {!isEditing && (
+      <button onClick={startEditingHandler}>Add Expense</button>
+      )
+    }
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
